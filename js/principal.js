e6298e27b252ef957760349b09876d13fa9b8666
Ana Menici <anamenici@gmail.com>
2018-10-01 20:52:19 -0300

    Aula 3
     Arrays, Loop e Estilos

--- js/principal.js
+++ js/principal.js
@@ -1,39 +1,42 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = document.querySelector(".info-altura");
-altura = tdAltura.textContent;
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
 
-var imc = peso / (altura * altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+    var tdAltura = paciente.querySelector(".info-altura");
+    altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if(peso >= 1000 || peso <=0){
-    console.log("Peso inválido!");
-    tdImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
+    if(peso >= 1000 || peso <= 0){
+        console.log("Peso inválido!");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(altura <= 0 || altura >= 3){
+        console.log("Altura inválida!");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / ( altura * altura);    
+        tdImc.textContent = imc.toFixed(2);
+    } 
 }
 
-if(altura <= 0 || altura >= 3){
-    console.log("Altura inválida!");
-    tdImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
-}
-
-if(pesoEhValido && alturaEhValida){
-    var imc = peso / ( altura * altura);    
-    tdImc.textContent = imc;
-} else {
-    tdImc.textContent = "Altura e/ou peso inválidos!";
-}
 
 
 
